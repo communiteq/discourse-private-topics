a9f47e607f22eee6e379ce9e61486fe78a38a43b
Richard <richard@communiteq.com>
2023-10-03 16:15:58 +0000

    Fix bug when category was changed; move to Glimmer

--- assets/javascripts/discourse/connectors/category-custom-security/private-topics.js
+++ assets/javascripts/discourse/connectors/category-custom-security/private-topics.js
@@ -1,27 +1,30 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service"
 import Group from "discourse/models/group";
 
-export default {
-  allGroups: null,
-  allowedGroupIds: [],
+export default class PrivateTopics extends Component {
+  @service siteSettings;
+  @tracked allGroups;
+  @tracked allowedGroupIds = [];
 
-  setupComponent(args, component) {
+  constructor() {
+    super(...arguments);
     Group.findAll().then((groups) => {
-      this.set("allGroups", groups);
+      this.allGroups = groups;
     });
-    var groups = this.category.custom_fields.private_topics_allowed_groups || "";
-    this.set("allowedGroupIds", groups.split(',').map(Number) || []);
-  },
-  actions: {
-    setAllowedGroups(groupIds) {
-      this.set("allowedGroupIds", groupIds);
-      this.set("category.custom_fields.private_topics_allowed_groups", groupIds.join(','));
-    },
-    onChangeSetting(value) {
-      this.set(
-        "category.custom_fields.private_topics_enabled",
-        value ? "true" : "false"
-      );
-    },
-  },
-};
+    this.allowedGroupIds = this.groupIds;
+  }
 
+  get groupIds() {
+    var g = this.allowedGroupIds; // necessary to trigger getter rerender
+    let groups = this.args.outletArgs.category.custom_fields.private_topics_allowed_groups || "";
+    return groups.split(',').filter(Boolean).map(Number) || [];
+  }
+
+  @action setAllowedGroups(groupIds) {
+    this.args.outletArgs.category.custom_fields.private_topics_allowed_groups = groupIds.join(',');
+    this.allowedGroupIds = groupIds;
+  }
+};
